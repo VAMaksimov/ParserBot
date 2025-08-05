4c913147cfe9a561226735be53ab48cf058b3170
VAMaksimov <mirpngfut@yandex.ru>
2025-08-05 13:50:31 +0000

    minor

---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+IMAGE_NAME="parser-bot"
+
+docker build -t ${IMAGE_NAME} .
--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,11 @@
-CONTAINER_NAME="parser-bot"
+IMAGE_NAME="parser-bot"
+CONTAINER_NAME="internship-bot"
 
 docker volume create bot-data
 
-docker run -d --name internship-bot \
+docker run -d --name ${CONTAINER_NAME} \
   -v bot-data:/app \
   -v bot.py:/app/bot.py \
   -v requirements.txt:app/requirements.txt \
   --restart unless-stopped \
-  ${CONTAINER_NAME}
+  ${IMAGE_NAME}
