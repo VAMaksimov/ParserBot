2648827270a043e695b16c16792e615cbdc39c51
Chopped Kanisha <choppedk@un-i1.kzn.21-school.ru>
2025-08-05 17:44:57 +0300

    minor

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.12-alpine
 WORKDIR /app
 
-# COPY requirements.txt .
+COPY requirements.txt .
 # COPY bot.py .
 
 RUN python -m venv venv
--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,16 @@
 IMAGE_NAME="parser-bot"
 CONTAINER_NAME="internship-bot"
 
-docker volume create bot-data
+if [ "$(docker volume ls -q -f name=bot-data | wc -l)" -eq "0" ]; then
+    echo "bot-data volume does not exist, creating"
+    docker volume create bot-data
+else
+    echo "bot-data volume already exists, skipping creation"
+fi
 
+# TO DO: properly mount bot.py
 docker run -d --name ${CONTAINER_NAME} \
   -v bot-data:/app \
   -v bot.py:/app/bot.py \
-  -v requirements.txt:app/requirements.txt \
   --restart unless-stopped \
   ${IMAGE_NAME}
